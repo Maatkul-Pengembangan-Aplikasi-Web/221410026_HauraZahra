02822215f695e04f870f20663b18174f0c4b43b1
Haurzaa <haurazahra185@gmail.com>
2024-11-24 17:14:22 +0700

    + edit, hapus, search

--- app/Http/Controllers/ProdiController.php
+++ app/Http/Controllers/ProdiController.php
@@ -8,9 +8,13 @@ use Illuminate\Http\Request;
 
 class ProdiController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $prodis = Prodi::orderBy('id', 'desc')->get();
+        $search = $request->input('search');
+        $prodis = Prodi::when($search, function ($query, $search) {
+            return $query->where('nama', 'like', '%' . $search . '%');
+        })->orderBy('id', 'desc')->get();
+
         return view('prodi.index', compact('prodis'));
     }
     
@@ -31,4 +35,32 @@ class ProdiController extends Controller
     
         return redirect()->route('/prodi')->with('success', 'Program Studi berhasil ditambahkan');
     }
+    public function edit($id)
+    {
+        $prodi = Prodi::findOrFail($id);
+        return view('prodi.edit', compact('prodi'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+        'nama' => 'required'
+        ]);
+
+        $prodi = Prodi::findOrFail($id);
+        $prodi->update([
+        'nama' => $request->nama
+        ]);
+
+    return redirect()->route('/prodi')->with('success', 'Program Studi berhasil diupdated');
+    }
+    public function delete($id)
+    {
+        $prodi = Prodi::findOrFail($id);
+        $prodi->delete();
+
+    return redirect()->route('/prodi')->with('success', 'Data Program Studi berhasil dihapus');
+    }
+    
 }
+
